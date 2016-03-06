e7b03e3d2cb5215f574c5755c19a69770272f4c9
boss913 <boss80913@gmail.com>
2016-03-07 00:13:23 +0800

    Update 258-AddDigits.cpp

--- 258-AddDigits.cpp
+++ 258-AddDigits.cpp
@@ -1,2 +1,21 @@
-test
-
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        
+        if(num%9==0&&num!=0)
+        return 9;
+        else if(num==0)
+        return 0;
+        else
+        
+       return num  % 9 ;
+            
+            
+            
+        
+        
+        
+        
+    }
+};
